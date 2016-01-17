a2f235a432ecdcd9b1b19501e6b0ff031c8731e6
Rare <windeer@163.com>
2016-01-17 11:04:18 +0800

    修改：首次提交 Json解析和序列化

---
+++ Assets/RareJson/TestJson.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rare;
+
+public class TestJson : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        // json对象构造示例
+        JsonObject root = new JsonObject();                                              
+        root["age"] = 25;
+        root["name"] = "rare";
+        JsonObject obj = new JsonObject();
+        root["person"] = obj;
+        obj["age"] = 1;
+        obj["name"] = "even";
+        JsonArray arr = new JsonArray();
+        arr[0] = "ComputerGrphic";
+        arr[1] = "Unity3D";
+        arr[2] = "Graphic";
+        root["books"] = arr;
+
+        // json取值
+        int a = root["age"];
+        Debug.Log("age:"+a.ToString());
+        string v = root["name"];
+        Debug.Log("name:" + v);
+
+        // save json and format
+        string strSerializeFile = Application.dataPath + "json/home.json";
+        RareJson.Serialize(root, strSerializeFile, true);
+
+        // json解析
+        string strJsonName = Application.dataPath + "json/Contents.json";
+        JsonNode node = RareJson.ParseJsonFile(strJsonName);
+
+        // json not format
+        strSerializeFile = Application.dataPath + "json/serialize.json";
+        RareJson.Serialize(node, strSerializeFile, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
